809fd3949bda6747aed8347eb5dcb89527b348b1
surajkasar <kasarsuraj15@gmail.com>
2016-09-13 06:43:24 -0700

    13-9-16

---
+++ js/scroll.js
@@ -0,0 +1,31 @@
+(function($) {
+    "use strict"; // Start of use strict
+
+    // for page scrolling-jQuery Easing plugin
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    });
+
+    // Closes the Responsive menu on menu item clicked
+    $('.navbar-collapse ul li a').click(function(){ 
+            $('.navbar-toggle:visible').click();
+    });
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
+
+})(jQuery); // End of use strict
